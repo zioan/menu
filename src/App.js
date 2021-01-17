841a045c80ff17869220dc2800a78bf0463ac125
zioan <zaharia.ioan@gmail.com>
2021-01-17 10:59:07 +0100

    dinamic generated buttons

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+console.log({ allCategories });
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
       <section className="menu section">
         <div className="title">
-          <h2>Our Menu</h2>
+          <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
